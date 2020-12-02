d5c7a0a31b8b0d40bfd376b6ae4dd58260ca6d9c
jesse.williamson <jesse.williamson@servicenow.com>
2020-12-02 09:49:09 -0800

    part 2 of day 1

--- Day1/Main.hs
+++ Day1/Main.hs
@@ -1,7 +1,7 @@
 import System.IO
 
 f :: [Integer] -> [Integer]
-f xs = take 1 [ x * y | x <- xs, y <- xs , (x + y) == 2020 ]
+f xs = take 1 [ x * y * z| x <- xs, y <- xs , z <- xs, (x + y + z) == 2020 ]
 
 main = do
 	handle <- openFile "./input.txt" ReadMode
